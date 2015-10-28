244b36fa562e0f678432c3f47a20760d60bf8217
zapante <zapante@miamioh.edu>
2015-10-28 03:18:45 -0400

    Basic Ruby Site with small database of basic movies from each genre

---
+++ NotIMDB/db/allmovies.rb
@@ -0,0 +1,34 @@
+b = Movie.new
+b.title = "Goonies"
+b.genre = "Adventure"
+b.save
+
+c = Movie.new
+c.title = "27 Dresses"
+c.genre = "Romance"
+c.save
+
+d = Movie.new
+d.title = "Old School"
+d.genre = "Comedy"
+d.save
+
+e = Movie.new
+e.title = "Alien"
+e.genre = "SciFi"
+e.save
+
+f = Movie.new
+f.title = "Tangled"
+f.genre = "Animation"
+f.save
+
+g = Movie.new
+g.title = "Karate Kid"
+g.genre = "Drama"
+g.save
+
+h = Movie.new
+h.title = "Fight Club"
+h.genre = "Action"
+h.save
