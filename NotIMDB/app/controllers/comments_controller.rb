79814cdaf1803701d7923a6b13ad05b7e17ed4cf
Emily Hughes <emilyhughes@Emilys-MacBook-Pro-2.local>
2015-12-01 17:51:47 -0500

    comment updates

--- NotIMDB/app/controllers/comments_controller.rb
+++ NotIMDB/app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
         def create
-                @movie = Movie.find(params[:movie_id])
+                @movie = Movie.find(params[:id])
                 @comment = @movie.comments.create!(params[:comment])
                 redirect_to @movie
         end
