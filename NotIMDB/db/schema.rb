f038fcdb3be1ed2ba033507c08ba24b15965fbc7
jerdacn2 <jerdacn2@miamioh.edu>
2015-12-01 21:15:07 -0500

    Schema

--- NotIMDB/db/schema.rb
+++ NotIMDB/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151201082132) do
+ActiveRecord::Schema.define(version: 20151201230455) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "movie_id"
+    t.text     "body"
+    t.String 	"user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["movie_id"], name: "index_comments_on_movie_id"
 
   create_table "movie_reqs", force: :cascade do |t|
     t.string   "title"
