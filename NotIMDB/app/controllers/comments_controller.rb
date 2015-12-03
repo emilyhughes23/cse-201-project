5de0c30882826413149ef5eb22b8f07806791433
Emily Hughes <emilyhughes@Emilys-MacBook-Pro-2.local>
2015-12-02 23:17:25 -0500

    comment controller

---
+++ NotIMDB/app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+	def create
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.create(comment_params)
+    redirect_to movie_path(@movie)
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :movie_id, :created_at, :updated_at)
+  end 
+end
